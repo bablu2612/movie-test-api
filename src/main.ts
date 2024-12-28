1b20e16f572dd1a69a9ddffcc320c80c26541174
bablu <bk147411@gmail.com>
2024-12-29 01:10:17 +0530

    added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -15,6 +16,13 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow all standard HTTP methods
     allowedHeaders: '*', // Allow all headers
   });
+
+
+  // Set body size limit for JSON payload
+  app.use(bodyParser.json({ limit: '50mb' })); // Increase limit to 50MB
+
+  // Set the file upload size limit
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   await app.listen(3000);
 }
